2fa0cd873ab563db4da073b765da0afe58393535
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-02-18 15:02:17 -0600

    created header and search bar components and folders for other future components

---
+++ instaclone/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const SearchBar = (props) => {
+    return (
+        <div>
+            Search Bar will go here.
+        </div>
+    )
+}
+
+
+export default SearchBar;
