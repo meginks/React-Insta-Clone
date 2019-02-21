0eb41ff46a7d055eb0eab1368cb8525e49dc13e5
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-02-21 08:20:38 -0600

    added postspage.js

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -1,45 +1,15 @@
-import React, { Component } from 'react';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar';
-import Post from './components/PostContainer/Post';
-import './App.css';
+import React from 'react'; 
+import PostsPage from './PostsPage';
 
-class App extends Component {
-  constructor() {
-    console.log('constructor running');
-    super(); 
-    this.state = {
-      postdata: [], 
-    };
-  }
-
-  componentDidMount() {
-    console.log('cdm running')
-    this.setState({ postdata: dummyData });
-  } 
-
-  incrementLikes = () => {
-    let likes = this.state.postdata.likes + 1; 
-    this.setState({ likes }); 
-  }
- 
-  render() {
-    return (
-      <div className="App">
-       <header className="header"> 
-          <p>instalogo</p>
-          <p>InstaClone</p> 
-          <SearchBar /> 
-          <p>Nav</p>
-          <p>heart</p>
-          <p>person</p>
-        </header> 
-        {this.state.postdata.map((post) => (
-        <Post key={post.imageUrl} postdata={post} />
-        ))}
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+        <div>
+            <PostsPage /> 
+        </div>
+        )
+    }
 }
+
 
 export default App;
